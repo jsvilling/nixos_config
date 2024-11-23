3e3f018f5401f9c1603b04b8bfd4e70e585ed0e8
Joshua Villing <jsvilling@gmail.com>
2024-11-23 09:06:46 +0100

    formatter for vscode

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -1,16 +1,34 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 {
-    programs.vscode = {
-        enable = true;
-        package = pkgs.vscodium;
-        extensions = with pkgs.vscode-extensions; [
-            asciidoctor.asciidoctor-vscode
-            bbenoist.nix
-            golang.go
-            haskell.haskell
-            justusadam.language-haskell
-            mkhl.direnv
-        ];
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      asciidoctor.asciidoctor-vscode
+      bbenoist.nix
+      jnoortheen.nix-ide
+      golang.go
+      haskell.haskell
+      justusadam.language-haskell
+      mkhl.direnv
+    ];
+    userSettings = {
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nil";
+      "nix.serverSettings" = {
+        "nil" = {
+          "diagnostics" = {
+            "ignored" = [
+              "unused_binding"
+              "unused_with"
+            ];
+          };
+          "formatting" = {
+            "command" = [ "nixpkgs-fmt" ];
+          };
+        };
+      };
     };
+  };
 }
