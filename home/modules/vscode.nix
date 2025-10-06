75d85153555c1954313fd79f64059870cd3b16d6
Joshua Villing <jsvilling@gmail.com>
2025-10-06 17:47:57 +0200

    Fix vscode warnings

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -2,38 +2,41 @@
 
 {
 
-home.packages = with pkgs; [
-  nil
-  nixpkgs-fmt
-];
+  home.packages = with pkgs; [
+    nil
+    nixpkgs-fmt
+  ];
 
-  programs.vscode = {
+  programs.vscode = with pkgs; {
     enable = true;
-    package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      asciidoctor.asciidoctor-vscode
-      bbenoist.nix
-      jnoortheen.nix-ide
-      golang.go
-      haskell.haskell
-      justusadam.language-haskell
-      mkhl.direnv
-    ];
+    package = vscodium;
 
-    userSettings = {
-      "editor.formatOnSave" = true;
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.serverSettings" = {
-        "nil" = {
-          "diagnostics" = {
-            "ignored" = [
-              "unused_binding"
-              "unused_with"
-            ];
-          };
-          "formatting" = {
-            "command" = [ "nixpkgs-fmt" ];
+    profiles.default = {
+      extensions = with vscode-extensions; [
+        asciidoctor.asciidoctor-vscode
+        bbenoist.nix
+        jnoortheen.nix-ide
+        golang.go
+        haskell.haskell
+        justusadam.language-haskell
+        mkhl.direnv
+      ];
+
+      userSettings = {
+        "editor.formatOnSave" = true;
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.serverSettings" = {
+          "nil" = {
+            "diagnostics" = {
+              "ignored" = [
+                "unused_binding"
+                "unused_with"
+              ];
+            };
+            "formatting" = {
+              "command" = [ "nixpkgs-fmt" ];
+            };
           };
         };
       };
