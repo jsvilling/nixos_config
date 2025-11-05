400161b7c863a6cd0dac270db1e7eb935cf1469d
Joshua Villing <jsvilling@gmail.com>
2025-11-05 09:50:42 +0100

    Include Iterm plist cfg

---
+++ home/modules/iterm/export-iterm-prefs.sh
@@ -0,0 +1,1 @@
+cp -f ~/Library/Preferences/com.googlecode.iterm2.plist ~/.config/home-manager/nixos_config/home/modules/iterm/com.googlecode.iterm2.plist
--- home/modules/iterm/iterm.nix
+++ home/modules/iterm/iterm.nix
@@ -1,7 +1,23 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  exportItermPrefs = pkgs.writeShellScriptBin "export-iterm-prefs" (builtins.readFile ./export-iterm-prefs.sh);
+in
 {
   home.packages = with pkgs; [
     iterm2
+    exportItermPrefs
   ];
+
+  home.file."Library/Preferences/com.googlecode.iterm2.plist.lnk" = {
+    source = ./com.googlecode.iterm2.plist;
+    enable = true;
+    force = true;
+  };
+
+  home.activation.itermPrefs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    cp -f ${./com.googlecode.iterm2.plist} ~/Library/Preferences/com.googlecode.iterm2.plist
+    killall cfprefsd 2>/dev/null
+    defaults read com.googlecode.iterm2
+  '';
 }
