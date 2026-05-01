87be77d4ca0ac36bbd646151d579c5be6bce5c4b
Joshua Villing <jsvilling@gmail.com>
2026-05-01 15:36:46 +0200

    Waybar

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -1,7 +1,22 @@
 { pkgs, ... }:
 
 let
-  src = ./hyprland.conf;
+  hyprLandSrc = ./hyprland.conf;
+  waybarSrc = ./waybar;
 in {
-  home.file.".config/hypr/hyprland.conf".source = src;
+  home.file = {
+    ".config/hypr/hyprland.conf".source = hyprLandSrc;
+    ".config/waybar" = {
+      source = waybarSrc;
+      recursive = true;
+    };
+  };
+  home.packages = with pkgs; [
+    kitty
+    waybar
+    wofi
+    dmenu
+    nwg-look
+    hyprpaper
+  ];
 }
